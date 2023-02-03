0e7142c1ecbb2e6a9b2150945428fce8a7012881
jinhyeokfang <jinhyeokfang@gmail.com>
2023-02-04 00:58:05 +0900

    dday

--- src/components/d-day.vue
+++ src/components/d-day.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="hello">
-    시간
+    D-{{ days }}
   </div>
 </template>
 
 <script>
+import dayjs from 'dayjs';
+
 export default {
-  name: 'DDay'
+  name: 'DDay',
+  data() {
+    return {
+      days: 0
+    }
+  },
+  mounted() {
+    const date1 = dayjs('2023-08-06');
+    const date2 = dayjs();
+    const dDays = date1.diff(date2, 'days') + 1;
+    this.days = dDays;
+  }
 }
 </script>
 
