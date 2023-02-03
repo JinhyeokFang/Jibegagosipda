8ced80bebe449a77745554b75fb9da8517c9db43
jinhyeokfang <jinhyeokfang@gmail.com>
2023-02-04 00:51:55 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DDay />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DDay from './components/d-day.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DDay
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/d-day.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    시간
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DDay'
+}
+</script>
+
+<style scoped>
+</style>
